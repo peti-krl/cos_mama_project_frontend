aac6afc050fc48d06308e2bbddeb68ed94453bdb
MARIELA NIKOLOVA <118596924+MariellaNik@users.noreply.github.com>
2024-03-12 11:12:49 +0200

    fix the logo import link

--- src/app-components/Navbar/Logo.jsx
+++ src/app-components/Navbar/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Logo() {
   return (
     <div className="logo-main">
-      <img src="../assets/Women’s club.png" alt="Your Logo" />
+      <img src="../../assets/Women’s club.png" alt="Your Logo" />
     </div>
   )
 }
