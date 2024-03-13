c81b5d1ac0bc5bd48346ac9c46ea3a183158b9d7
Radko Mishev <rm.mishev@gmail.com>
2024-03-13 21:11:59 +0200

    (feat): Add pink block on page

--- src/app-components/SignIn/SignInPage.tsx
+++ src/app-components/SignIn/SignInPage.tsx
@@ -3,7 +3,8 @@ import { SignInForm } from "./SignInForm";
 
 export const SignInPage = () => {
   return (
-    <div className="flex justify-center items-center  h-screen">
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-1/3 h-full bg-pink-200"></div>
       <AuthLayout>
         <SignInForm />
       </AuthLayout>
