8c0f5efaec2be97d06189fa6be2d01f5a703c704
MagdalenaZh <magdalenazheleva1@gmail.com>
2024-04-11 11:38:40 +0300

    add blog section and images to page

--- src/app-components/home/BlogHomepage.tsx
+++ src/app-components/home/BlogHomepage.tsx
@@ -15,57 +15,29 @@ const BlogHomepage = () => {
     return (
         <>
             <div className='events'>
-                <p className='font-bold mt-5 mb-2 flex justify-center align-center'>Upcoming events</p>
+                <p className='font-bold mt-5 mb-2 flex justify-center align-center'>Blogs</p>
                 <p>
                     <div className='inline flex flex row justify-items-center' >
                         <Card>
                             <CardHeader className='w-[280px]'>
-                                <img src="/event1.jpg" style={{ height: '110px' }} />
-                                <CardTitle>Educational event - Breastfeeding Workshop</CardTitle>
+                                <img src="/blogs/blog-1.jpg" style={{ height: '110px' }} />
+                                <CardTitle>Navigating Social Events During Pregnancy: A Guide to Enjoying Gatherings</CardTitle>
                             </CardHeader>
-                            <CardContent>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <FaLocationArrow style={{ marginRight: '8px', fontSize: '0.7rem' }} />
-                                    <p className='text-sm'>Korner Coworking & Event space</p>
-                                </div>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <FaClock style={{ marginRight: '8px', fontSize: '0.7rem' }} />
-                                    <p className='text-sm'> 6pm - 7:00pm </p>
-                                </div>
-                            </CardContent>
+                        
                         </Card>
                         <Card className='w-[280px]'>
                             <CardHeader >
-                                <img src="/event2.jpg" style={{ height: '110px' }} />
-                                <CardTitle>Entertainment event - Mamma Yoga</CardTitle>
+                                <img src="/blogs/blog-2.png" style={{ height: '110px' }} />
+                                <CardTitle>The importance of social support in pregnancy and ways to connect with others</CardTitle>
                             </CardHeader>
-                            <CardContent>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <FaLocationArrow style={{ marginRight: '8px', fontSize: '0.7rem' }} />
-                                    <p className='text-sm'>Yoga Vibe East</p>
-                                </div>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <FaClock style={{ marginRight: '8px', fontSize: '0.7rem' }} />
-                                    <p className='text-sm'> 6pm - 7:20pm (workdays) </p>
-                                </div>
-                            </CardContent>
+                            
                         </Card>
                         <Card className='w-[280px]'>
                             <CardHeader >
-                                <img src="/event3.jpg" style={{ height: '110px' }} />
-                                <CardTitle>Networking event - DIY Mocktails</CardTitle>
+                                <img src="/blogs/blog-3.jpg" style={{ height: '110px' }} />
+                                <CardTitle>Hobbies While Pregnant: 14 New Low-Energy + Fun Ideas</CardTitle>
                             </CardHeader>
-                            <CardContent>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <FaLocationArrow style={{ marginRight: '8px', fontSize: '0.7rem' }} />
-                                    <p className='text-sm'>Cafe 1920</p>
-                                </div>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <FaClock style={{ marginRight: '8px', fontSize: '0.7rem' }} />
-                                    <p className='text-sm'> 6pm - 8pm (workdays) </p>
-                                </div>
-
-                            </CardContent>
+                           
                         </Card>
                     </div>
                 </p>
