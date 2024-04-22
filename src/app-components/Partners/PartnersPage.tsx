d4455b4a5e7e0b760479ac8fc52ea08e46f013e9
MagdalenaZh <magdalenazheleva1@gmail.com>
2024-04-22 21:57:31 +0300

    add buttons to partners page

--- src/app-components/Partners/PartnersPage.tsx
+++ src/app-components/Partners/PartnersPage.tsx
@@ -26,29 +26,31 @@ const PartnersPage = () => {
                     <div className="border-b-2 border-black w-[300px] mx-auto mb-5 mt-5"></div>
                      <div className="flex flex-col justify-center items-center text-center my-4">
                      <p className="text-6xl  mt-4 font-bold">5000</p>
-                    <span className="text-3xl text-base font-medium">Yearly</span>
+                     <span className="text-2xl font-medium">Yearly</span>
                     </div>
         </div>
     
         <ul className="flex-1 p-5">
             <li className="mb-3 flex items-center">
-                 <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+                 <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Newsletter
             </li>
              <li className="mb-3 text-l flex items-center">
-                 <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+                 <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
                 Logo on our website
             </li>
             <li className="mb-3 flex items-center">
-                 <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+                 <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
                 Mention in article (without link)
             </li>
             <li className="mb-3 flex items-center">
-                 <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+                 <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
                 Merch during events
             </li>
          </ul>
-   
+         <div className="flex justify-center items-center p-5">
+         <Button className="w-[120px] h-[50px] mx-1">Choose</Button>
+        </div>
     </div>
     <div className="w-[400px] h-[650px] bg-white mx-8 my-5 rounded-2xl border-2 border-red-400 shadow-lg flex flex-col">
         <div className="p-5">
@@ -56,84 +58,90 @@ const PartnersPage = () => {
             <div className="border-b-2 border-black w-[300px] mx-auto mb-5 mt-5"></div>
             <div className="flex flex-col justify-center items-center text-center my-4">
             <p className="text-6xl  mt-4 font-bold">7000</p>
-            <span className="text-3xl text-base font-medium">Yearly</span>
+            <span className="text-2xl font-medium">Yearly</span>
         </div>
     </div>
     <ul className="flex-1 p-5">
         <li className="mb-3 flex items-center">
-            <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+            <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
              Newsletter
         </li>
         <li className="mb-3 flex items-center">
-            <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+            <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Logo on our website
         </li>
         <li className="mb-3 flex items-center">
-            <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+            <span className="flex-none  text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Link in an article
         </li>
         <li className="mb-3 flex items-center">
-            <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+            <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Merch during events
         </li>
         <li className="mb-3 flex items-center">
-             <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+             <span className="flex-none text-green-500 text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Interactive workshop
         </li>
     </ul>
+        <div className="flex justify-center items-center p-5">
+         <Button className="w-[120px] h-[50px] mx-1">Choose</Button>
+        </div>
     </div>
     
-    <div className="w-[400px] h-[600px] bg-white mx-8 my-5 rounded-2xl border-2 border-red-400 shadow-lg flex flex-col">
+    <div className="w-[400px] h-[600px]  bg-white mx-8 my-5 rounded-2xl border-2 border-red-400 shadow-lg flex flex-col">
     <div className="p-5">
         <h3 className="text-center font-bold text-2xl ">PLATINUM</h3>
         <div className="border-b-2 border-black w-[300px] mx-auto mb-5 mt-5"></div>
         <div className="flex flex-col justify-center items-center text-center my-4">
     <p className="text-6xl  mt-4 font-bold">12000</p>
-    <span className=" text-3xl text-base font-medium">Yearly</span>
+    <span className="text-2xl font-medium">Yearly</span>
     </div>
 
     </div>
     <ul className="flex-1 p-5">
         <li className="mb-3 flex items-center">
-            <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+            <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Newsletter
         </li>
         <li className="mb-3 flex items-center">
-             <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+             <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Logo on our website
         </li>
         <li className="mb-3 flex items-center">
-             <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+             <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Whole article for the company
         </li>
         <li className="mb-3 flex items-center">
-             <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+             <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Merch during events
         </li>
         <li className="mb-3 flex items-center">
-            <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+            <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Exclusive event for the company
         </li>
         <li className="mb-3 flex items-center">
-             <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+             <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Banner on spot
         </li>
         <li className="mb-3 flex items-center">
-             <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+             <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Post on Instagram
         </li>
-        <li className="mb-3 flex items-center">
-             <span className="flex-none bg-white mr-2 font-bold flex justify-center items-center">✓</span>
+        <li className="flex items-center">
+             <span className="flex-none text-green-500 bg-white mr-2 font-bold flex justify-center items-center">✓</span>
             Post on Facebook
         </li>
     </ul>
+        <div className="flex justify-center items-center">
+         <Button className="w-[120px] h-[50px]">Choose</Button>
+        </div>
     </div>
     </div>
           
     <div className="bg-red-400 p-6 sm:p-12 rounded-2xl shadow-lg mx-auto mt-[100px] mb-[50px] w-[1300px] flex flex-wrap justify-center items-center">
         <Input type="name" placeholder="Name" className="w-[400px] h-[40px] text-gray-700 bg-white mr-14"  />
         <Input type="email" placeholder="Email" className="w-[400px] h-[40px] text-gray-700 bg-white mr-14" />
-        <Button className="w-[80px] h-[40px] mx-1">Send</Button>
+        <Button className="w-[100px] h-[40px] mx-1">Send</Button>
     </div>
 
         <h1 className="text-2xl  mt-10 mb-4 text-center">MamaBuddy.com</h1>
